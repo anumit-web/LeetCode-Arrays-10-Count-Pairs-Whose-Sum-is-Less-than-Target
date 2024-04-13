e82f0ebd9633f04ca968b56ce014b067c452f81d
Anumit Jooloor <anumit@gmail.com>
2024-04-13 13:34:03 -0700

    Add files

---
+++ Main.java
@@ -0,0 +1,32 @@
+/*
+/usr/local/Cellar/openjdk/21.0.2/bin/javac *.java
+/usr/local/Cellar/openjdk/21.0.2/bin/java Main
+*/
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Number of Employees Who Met the Target");
+
+        Solution solution = new Solution();
+
+        int[] nums1 = { -1,1,2,3,1 };
+        List<Integer> nums1_list = new ArrayList<Integer>();
+        nums1_list.add90;
+
+
+        int extraCandies1 = 3;
+        List<Boolean> result1= solution.countPairs(candies1, extraCandies1);
+        System.out.println("Count = " + result1);
+
+        int[] candies2 = { 4,2,1,1,2 };
+        int extraCandies2 = 1;
+        List<Boolean> result2 = solution.kidsWithCandies(candies2, extraCandies2);
+        System.out.println("Count = " + result2);
+
+
+    }
+}
---
+++ Solution.java
@@ -0,0 +1,25 @@
+/*
+rm *.class
+/usr/local/Cellar/openjdk/21.0.2/bin/javac *.java
+/usr/local/Cellar/openjdk/21.0.2/bin/java Main
+*/
+
+import java.io.*;
+import java.util.*;
+
+class Solution {
+
+    public int countPairs(List<Integer> nums, int target) {
+
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums.get(i) + nums.get(j) < target)
+                    count++;
+            }
+        }
+        
+        return count;
+    }
+
+}
